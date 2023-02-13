c8b60ceb28d4614621f61fb01cf37f38c990dd6e
Alexey Ermolaev <descendended@gmail.com>
2023-02-13 17:03:40 +0300

    m1l5-coroutines (#3)
    Co-authored-by: a.ermolaev <a.ermolaev@vk.team>

---
+++ m1l5-coroutines/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("jvm")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    testImplementation(kotlin("test-junit"))
+}
