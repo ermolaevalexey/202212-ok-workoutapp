f0b97a84da3bd1d18cc984a35ea191e2b30c1637
desce <descendended@gmail.com>
2023-03-01 21:48:59 +0300

    m2-openapi - tests for transport models - responses, fixes

---
+++ ok-workoutapp-api-v1-jackson/src/test/kotlin/RequestSerializationTest.kt
@@ -0,0 +1,88 @@
+package ru.otus.otuskotlin.workoutapp.api.v1
+
+import ru.otus.otuskotlin.workoutapp.api.v1.models.*
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+class RequestSerializationTest {
+  private val createRequest = WorkoutCreateRequest(
+    requestId = "123",
+    debug = Debug(
+      mode = RequestDebugMode.TEST,
+      stub = RequestDebugStubs.BAD_TITLE
+    ),
+    workout = WorkoutBase(
+      title = "Workout with Dumbbells",
+      description = "Simple test workout",
+      type = WorkoutType.ARMS,
+      equipment = Equipment.DUMBBELLS,
+      content = WorkoutBaseContent(
+        video = "https://www.youtube.com/watch?v=2fCKd-BQOhw",
+        steps = listOf<WorkoutStep>(
+          WorkoutStep(
+            title = "Поднимите гантели",
+            description = "Описание шага"
+          ),
+          WorkoutStep(
+            title = "Опустите гантели",
+            description = "Описание отпуска гантелей"
+          )
+        )
+      )
+    )
+  )
+
+  private val searchRequest = WorkoutSearchRequest(
+    requestId = "128",
+    debug = Debug(
+      mode = RequestDebugMode.TEST,
+      stub = RequestDebugStubs.BAD_TITLE
+    ),
+    params = WorkoutSearchRequestPayload(
+      groupBy = WorkoutSearchRequestPayloadGroupBy(
+        workoutType = false,
+        equipment = true
+      )
+    )
+  )
+
+  @Test
+  fun serializeCreateRequest() {
+    val json = apiV1Mapper.writeValueAsString(createRequest)
+
+    assertContains(json, Regex("\"requestId\":\\s*\"123\""))
+    assertContains(json, Regex("\"title\":\\s*\"Workout with Dumbbells\""))
+    assertContains(json, Regex("\"mode\":\\s*\"test\""))
+    assertContains(json, Regex("\"stub\":\\s*\"badTitle\""))
+    assertContains(json, Regex("\"requestType\":\\s*\"workoutCreate\""))
+    assertContains(json, Regex("\"video\":\\s*\"https://www.youtube.com/watch\\?v=2fCKd-BQOhw\""))
+  }
+
+  @Test
+  fun serializeSearchRequest() {
+    val json = apiV1Mapper.writeValueAsString(searchRequest)
+
+    assertContains(json, Regex("\"requestId\":\"128\""))
+    assertContains(json, Regex("\"mode\":\"test\""))
+    assertContains(json, Regex("\"stub\":\"badTitle\""))
+    assertContains(json, Regex("\"requestType\":\\s*\"workoutSearch\""))
+    assertContains(json, Regex("\"workoutType\":\\s*false"))
+    assertContains(json, Regex("\"equipment\":\\s*true"))
+  }
+
+  @Test
+  fun deserializeCreateRequest() {
+    val json = apiV1Mapper.writeValueAsString(createRequest)
+    val obj = apiV1Mapper.readValue(json, Request::class.java) as WorkoutCreateRequest
+
+    assertEquals(createRequest, obj)
+  }
+
+  @Test
+  fun deserializeSearchRequest() {
+    val json = apiV1Mapper.writeValueAsString(searchRequest)
+    val obj = apiV1Mapper.readValue(json, Request::class.java) as WorkoutSearchRequest
+
+    assertEquals(searchRequest, obj)
+  }
+}
---
+++ ok-workoutapp-api-v1-jackson/src/test/kotlin/ResponseSerializationTest.kt
@@ -0,0 +1,127 @@
+package ru.otus.otuskotlin.workoutapp.api.v1
+
+import ru.otus.otuskotlin.workoutapp.api.v1.models.*
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+class ResponseSerializationTest {
+  private val workout1 = WorkoutCreateResponsePayload(
+    id = "567",
+    title = "Workout with Dumbbells",
+    description = "Simple test workout",
+    type = WorkoutType.ARMS,
+    equipment = Equipment.DUMBBELLS,
+    content = WorkoutBaseContent(
+      video = "https://www.youtube.com/watch?v=2fCKd-BQOhw",
+      steps = listOf<WorkoutStep>(
+        WorkoutStep(
+          title = "Поднимите гантели",
+          description = "Описание шага"
+        ),
+        WorkoutStep(
+          title = "Опустите гантели",
+          description = "Описание отпуска гантелей"
+        )
+      ),
+    ),
+    rating = 0.0,
+    feedback = emptyList()
+  )
+
+  private val workout2 = WorkoutCreateResponsePayload(
+    id = "567",
+    title = "Workout with Bars",
+    description = "Simple test bars",
+    type = WorkoutType.ARMS,
+    equipment = Equipment.BARS,
+    content = WorkoutBaseContent(
+      video = "https://www.youtube.com/watch?v=2fCKd-BQOhw",
+      steps = listOf<WorkoutStep>(
+        WorkoutStep(
+          title = "Подтянитесь",
+          description = "Описание шага"
+        ),
+        WorkoutStep(
+          title = "Опуститесь",
+          description = "Описание отпуска гантелей"
+        )
+      ),
+    ),
+    rating = 5.0,
+    feedback = listOf(
+      Feedback(
+        user = FeedbackUser(id = "1234-456", name = "Василий Пупкин"),
+        rating = 4.5,
+        review = "Очень хорошая тренировка"
+      ),
+      Feedback(
+        user = FeedbackUser(id = "1234-456", name = "Серсея Ланнистер"),
+        rating = 2.5,
+        review = "Слишком тяжело"
+      )
+    )
+  )
+
+
+  private val createResponse = WorkoutCreateResponse(
+    requestId = "123",
+    result = Result.SUCCESS,
+    errors = emptyList<Error>(),
+    workout = workout1
+  )
+
+  private val searchResponse = WorkoutSearchResponse(
+    requestId = "128",
+    result = Result.SUCCESS,
+    workout = WorkoutSearchResponsePayload(
+      groups = listOf(
+        WorkoutSearchResponsePayloadGroupsInner(
+          groupName = Equipment.DUMBBELLS.toString(),
+          workouts = listOf(workout1)
+        ),
+        WorkoutSearchResponsePayloadGroupsInner(
+          groupName = Equipment.BARS.toString(),
+          workouts = listOf(workout2)
+        )
+      )
+    )
+  )
+
+  @Test
+  fun serializeCreateResponse() {
+    val json = apiV1Mapper.writeValueAsString(createResponse)
+
+    assertContains(json, Regex("\"requestId\":\\s*\"123\""))
+    assertContains(json, Regex("\"id\":\\s*\"567\""))
+    assertContains(json, Regex("\"title\":\\s*\"Workout with Dumbbells\""))
+
+    assertContains(json, Regex("\"responseType\":\\s*\"workoutCreate\""))
+  }
+
+  @Test
+  fun serializeSearchResponse() {
+    val json = apiV1Mapper.writeValueAsString(searchResponse)
+
+    assertContains(json, Regex("\"requestId\":\"128\""))
+    assertContains(json, Regex("\"title\":\\s*\"Workout with Bars\""))
+    assertContains(json, Regex("\"responseType\":\\s*\"workoutSearch\""))
+    assertContains(json, Regex("\"groupName\":\\s*\"Dumbbells\""))
+  }
+
+  @Test
+  fun deserializeCreateResponse() {
+    val json = apiV1Mapper.writeValueAsString(createResponse)
+    val obj = apiV1Mapper.readValue(json, Response::class.java) as WorkoutCreateResponse
+
+    assertEquals(createResponse, obj)
+  }
+
+  @Test
+  fun deserializeSearchResponse() {
+    val json = apiV1Mapper.writeValueAsString(searchResponse)
+    val obj = apiV1Mapper.readValue(json, Response::class.java) as WorkoutSearchResponse
+
+    assertEquals(searchResponse, obj)
+  }
+}
