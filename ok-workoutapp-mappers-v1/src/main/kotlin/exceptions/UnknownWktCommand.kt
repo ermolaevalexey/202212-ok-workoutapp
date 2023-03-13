00a74ed0ad5f84d8574d3c046353a93132f1bd37
desce <descendended@gmail.com>
2023-03-13 13:47:44 +0300

    m2-openapi - cr fixes

--- ok-workoutapp-mappers-v1/src/main/kotlin/exceptions/UnknownWktCommand.kt
+++ ok-workoutapp-mappers-v1/src/main/kotlin/exceptions/UnknownWktCommand.kt
@@ -2,4 +2,4 @@ package ru.otus.otuskotlin.workoutapp.mappers.v1.exceptions
 
 import ru.otus.otuskotlin.workoutapp.common.models.WktCommand
 
-class UnknownWktCommand(command: WktCommand) : Throwable("Wrong command $command at mapping toTransport stage")
+class UnknownWktCommand(command: WktCommand) : Exception("Wrong command $command at mapping toTransport stage")
